c1d0d7b202ccf28f7886a7a4e3a01807ee7c3f58
Felipe Rooke <felipe@rooke.com.br>
2018-09-13 16:09:40 -0300

    Criando Lista com todos os IDs coletados a partir do bbox

---
+++ helpers/extractor_list_all_ids_bbox.py
@@ -0,0 +1,14 @@
+# recebe chaves do twitter e o id do usuario e limite de friends para coleta
+import conf
+import subprocess
+import logging
+import os
+from os import walk
+import socket
+
+
+def extrai_lista():
+
+
+    _, _, users_coletados = walk(dir_friends).next()
+    
